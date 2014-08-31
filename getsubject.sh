49e7994dc800620c4b0cbb9521c96173fbfd5a0d
mikesimarta <michael.simarta@gmail.com>
2014-09-01 00:34:34 +1000

    Add subjects that users tutor as well

--- getsubject.sh
+++ getsubject.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Created by Michael Simarta
 # Template to find subjects
-acc $@ | tr ' ' '\n' | egrep "Student" | cut -d'_' -f1 | tr '\n' ' '
+acc $@ | tr ' ' '\n' | egrep "[A-Z]{4}[0-9]{4}_(Student|Tutor)" | cut -d'_' -f1 | tr '\n' ' '
