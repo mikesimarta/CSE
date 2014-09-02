afce5beb69a793b1a71f45981fe01b9633a64bd1
mikesimarta <michael.simarta@gmail.com>
2014-09-02 13:38:07 +1000

    Update getallzid.sh

--- getallzid.sh
+++ getallzid.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # Created by Michael Simarta and Sarah Bennett
 # Usage: ./getallzid.sh (filename)
+# Need getusername.sh to be executable
 # Get all names from a list of zID.
 file=`cat $1`
-for user in $file; do ./getusername.sh "z$user"; done
+for user in $file
+do
+   zNum=`echo "$user" | cut -d' ' -f1 | egrep [0-9]{7}`
+   zID=`echo "$zNum" | egrep z[0-9]{7}`
+   if [ "$zID" != "" ]
+   then
+      ./getusername.sh "$zID"
+   elif [ "$zNum" != "" ]
+   then
+      ./getusername.sh "z$zNum"
+   fi
+   echo "$user"
+done
