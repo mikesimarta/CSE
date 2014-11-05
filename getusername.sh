3406bdc2bc9980be44a351c0ee9f3b5f1fa7b060
mikesimarta <michael.simarta@gmail.com>
2014-11-05 23:54:46 +1100

    Update getusername.sh

--- getusername.sh
+++ getusername.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Created by Michael Simarta and Sarah Bennett
 # Gets the Full Name from an alias (zID, CSE Name)
-acc $@ | grep "[^r] Name" | cut -f2 -d':'
+name=`acc $@ | grep "[^r] Name" | cut -f2 -d':'`
+echo -n "$name  "
