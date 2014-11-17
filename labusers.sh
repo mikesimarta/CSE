20f71ce8ee7b1c2366a9f8a112c66c18460667dc
mikesimarta <michael.simarta@gmail.com>
2014-11-18 09:21:39 +1100

    Update labusers.sh
    Increase efficiency
    Properly display those logged into multiple computers

--- labusers.sh
+++ labusers.sh
@@ -3,35 +3,33 @@
 # Usage: ./labusers.sh
 # Print names and locations of who is using the lab computers and where
 
-filter=`lab -A | egrep -v ^"Lab " | egrep -v ^"Cannot get table"|
- cut -d':' -f1,4 | cut -d' ' -f1,2 | sed 's/ /-/g'`
+filter=`lab drum tuba flute banjo oud clavier guan mabu sanhu erhu viola cello | egrep -v ^"Lab " | egrep -v ^"Cannot get table"|
+cut -d':' -f1,4 | cut -d' ' -f1,2 | sed 's/ /-/g'`
 for line in $filter
 do
    username=`echo "$line" | cut -d'-' -f2`
    lab=`echo "$line" | cut -d'-' -f1`
-   
    if [ "$username" != "" ]
    then
       labName=`echo "$lab" | tr -d [:0-9]`
-      logintime=`lab $labName | grep "$username" | cut -d';' -f2`
+      logintime=`lab $labName | grep "$lab" | grep "$username" | cut -d';' -f2`
       name=`./getusername.sh "$username"`
       subject=`./getsubject.sh "$username"`
-      new=`echo "$lab$name $logintime $subject"`
-
+      new=`echo "$lab$name $username $logintime $subject"`
       if [ $# -gt 0 ]
       then
          greped=`echo "$new" | grep $1`
          if [ $# -gt 1 ]
          then
             greped=`echo "$greped" | grep $2`
-         fi
-         if [ $# -gt 2 ]
-         then
-            greped=`echo "$greped" | grep $3`
+            if [ $# -gt 2 ]
+            then
+               greped=`echo "$greped" | grep $3`
+               if [ $# -gt 3 ]
+               then
+                  greped=`echo "$greped" | grep $4`
+               fi
             fi
-         if [ $# -gt 3 ]
-         then
-            greped=`echo "$greped" | grep $4`
          fi
          new=`echo "$greped"`
       fi
@@ -41,3 +39,4 @@ do
       fi
    fi
 done
+
